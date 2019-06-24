06949391f930d7621902cd797b027df4ebe15f38
Mark Tkachenko <m.tkachenko@cargoonline.ru>
2019-06-24 21:42:04 +0000

    paket as global tool

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-mono .paket/paket.bootstrapper.exe
-mono .paket/paket.exe install
+dotnet tool install paket -g
+paket install
 dotnet tool install fake-cli --tool-path .fake
 .fake/fake run build.fsx
