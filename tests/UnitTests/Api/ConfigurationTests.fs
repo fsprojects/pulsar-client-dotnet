4b17c71fe089b88819f0a044b2853e8bc148a694
Mikhail Prokushenkov <m.prokushenkov@gmail.com>
2019-07-04 21:51:39 +0300

    Add property 'MaxPendingMessages' into 'ProducerConfiguration'

---
+++ tests/UnitTests/Api/ConfigurationTests.fs
@@ -0,0 +1,27 @@
+﻿namespace Pulsar.Client.UnitTests.Api
+
+open System
+open Expecto
+open Expecto.Flip
+open Pulsar.Client.Api
+open Pulsar.Client.UnitTests
+
+module ConfigurationTests =
+
+    let private incorrectDefault propertyName =
+        sprintf "Incorrect default value for '%s'" propertyName
+
+    [<Tests>]
+    let tests = 
+
+        testList "ConfigurationTests" [
+
+            test "ProducerConfiguration should have correct defaults" {
+                let config = ProducerConfiguration.Default
+
+                config.Topic |> Expect.equal (incorrectDefault "Topic") String.Empty
+                config.ProducerName |> Expect.equal (incorrectDefault "ProducerName") String.Empty
+                config.MaxPendingMessages |> Expect.equal (incorrectDefault "MaxPendingMessages") 1000
+            }
+
+        ]
