511c78b984f024ea4046121c9396ec2ae6893055
Mikhail Prokushenkov <m.prokushenkov@gmail.com>
2019-08-11 21:42:27 +0300

    2. Add batch implementations skeletons

--- src/Pulsar.Client/Api/Batching.fs
+++ src/Pulsar.Client/Api/Batching.fs
@@ -6,7 +6,7 @@ open System
 type IBatchMessageContainer =
 
     /// Clear the message batch container.
-    abstract member Clear : unit
+    abstract member Clear : unit -> unit
 
     /// Check the message batch container is empty.
     abstract member IsEmpty : bool with get
@@ -28,7 +28,60 @@ type IBatchMessageContainer =
 type IBatcherBuilder =
 
     /// Creates Default batch message container.
-    abstract member Default : IBatchMessageContainer
+    abstract member Default : IBatchMessageContainer with get
 
     /// Key based batch message container.
-    abstract member KeyBased : IBatchMessageContainer
+    abstract member KeyBased : IBatchMessageContainer with get
+
+    abstract member Build : unit -> IBatchMessageContainer
+
+
+type internal DefaultBatchMessageContainer =
+
+    new() = DefaultBatchMessageContainer()
+
+    interface IBatchMessageContainer with
+
+        member __.Clear() = raise(NotImplementedException())
+
+        member __.IsEmpty with get() = raise(NotImplementedException())
+
+        member __.NumMessagesInBatch with get() = raise(NotImplementedException())
+
+        member __.CurrentBatchSize with get() = raise(NotImplementedException())
+
+        member __.Discard exn = raise(NotImplementedException())
+
+        member __.IsMultiBatches with get() = raise(NotImplementedException())
+
+
+type internal KeyBasedBatchMessageContainer =
+
+    new() = KeyBasedBatchMessageContainer()
+
+    interface IBatchMessageContainer with
+
+        member __.Clear() = raise(NotImplementedException())
+
+        member __.IsEmpty with get() = raise(NotImplementedException())
+
+        member __.NumMessagesInBatch with get() = raise(NotImplementedException())
+
+        member __.CurrentBatchSize with get() = raise(NotImplementedException())
+
+        member __.Discard exn = raise(NotImplementedException())
+
+        member __.IsMultiBatches with get() = raise(NotImplementedException())
+
+
+type internal BatcherBuilder(defaultContainer : IBatchMessageContainer, keyBasedContainer : IBatchMessageContainer) =
+
+    new() = BatcherBuilder(DefaultBatchMessageContainer(), KeyBasedBatchMessageContainer())
+
+    interface IBatcherBuilder with
+
+        member val Default = defaultContainer with get
+
+        member val KeyBased = keyBasedContainer with get
+
+        member __.Build() = raise(NotImplementedException())
