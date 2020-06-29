d9422c6aef3dce292c1c435f67dd5a75a2fbcdbc
rwilkinson <rwilkinson@mercuria.com>
2020-06-29 16:21:23 +0100

    Avoid changing avro source code

--- src/Pulsar.Client/Schema/AvroSchema.fs
+++ src/Pulsar.Client/Schema/AvroSchema.fs
@@ -53,7 +53,12 @@ type internal AvroSchema<'T> private (schema: Schema, avroReader: DatumReader<'T
         if avroReader :? SpecificDatumReader<'T> then
             AvroSchema(schema, SpecificDatumReader(writtenSchema, schema), avroWriter) :> ISchema<'T>
         else
-            AvroSchema(schema, ReflectReader<'T>(writtenSchema, schema), avroWriter) :> ISchema<'T>
+            //Avro doesnt figure that the written classname might be different from the reader classname
+            //Seems like it might be a bug in ReflectReader, but this works around that
+            let cache = ClassCache()
+            if not (writtenSchema.Fullname.Equals(schema.Fullname)) then
+                cache.LoadClassCache(typeof<'T>, writtenSchema);
+            AvroSchema(schema, ReflectReader<'T>(writtenSchema, schema, cache), avroWriter) :> ISchema<'T>
         
 type internal GenericAvroSchema(topicSchema: TopicSchema) =
     inherit ISchema<GenericRecord>()    
