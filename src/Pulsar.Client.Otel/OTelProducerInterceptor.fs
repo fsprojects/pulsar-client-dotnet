5fe6605c5d7489d7e217d044ec25f6d5799ba3ab
fjod <fourjustoneday@yandex.ru>
2021-04-26 14:49:44 +0300

    basic functionality for Open Telemetry, need info about Propagation

---
+++ src/Pulsar.Client.Otel/OTelProducerInterceptor.fs
@@ -0,0 +1,52 @@
+﻿module Pulsar.Client.Otel.OTelProducerInterceptor
+
+open System.Diagnostics
+open OpenTelemetry
+open Pulsar.Client.Api
+open OpenTelemetry.Context.Propagation
+open Pulsar.Client.Common
+//https://github.com/open-telemetry/opentelemetry-dotnet/blob/main/src/OpenTelemetry.Api/README.md#instrumenting-a-libraryapplication-with-net-activity-api
+type OTelProducerInterceptor<'T>() =
+    let Propagator = Propagators.DefaultTextMapPropagator
+    
+    member this.activitySource : ActivitySource = new ActivitySource("pulsar.producer")
+    interface IProducerInterceptor<'T> with        
+        member this.BeforeSend(producer, message) =           
+            let name = producer.Topic + " send"            
+            let activity = this.activitySource.StartActivity(name,ActivityKind.Producer)   
+            if activity <> null then     //If there are no listeners interested in this activity, the activity above will be null <..> Ensure that all subsequent calls using this activity are protected with a null check.           
+                if activity.IsAllDataRequested = true then // It is highly recommended to check activity.IsAllDataRequested, before populating any tags which are not readily available. IsAllDataRequested is the same as Span.IsRecording and will be false when samplers decide to not record the activity, and this can be used to avoid any expensive operation to retrieve tags.
+                   //https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/messaging.md
+                   activity.SetTag("messaging.system", "pulsar") |> ignore
+                   activity.SetTag("messaging.destination_kind", "topic") |> ignore
+                   activity.SetTag("messaging.destination", producer.Topic) |> ignore
+                   activity.SetTag("messaging.operation", "BeforeSend") |> ignore
+                   //add propagator here
+                   let contextToInject = activity.Context;
+                   Propagator.Inject(new PropagationContext(contextToInject, Baggage.Current), message, this.InjectTraceContextIntoBasicProperties);
+                    
+                   activity.Stop()
+            message
+        member this.Close() = () //stop activities from before& ack ?
+        member this.Eligible _ = true
+        member this.OnSendAcknowledgement(producer, _, messageId, ``exception``) =           
+            match ``exception`` with
+                | null ->
+                      let activity = this.activitySource.StartActivity("exception",ActivityKind.Producer)                      
+                     //https://github.com/open-telemetry/opentelemetry-specification/blob/main/specification/trace/semantic_conventions/exceptions.md
+                      if ``exception`` <> null then                        
+                          activity.SetTag("exception.type", ``exception``.Source) |> ignore
+                          activity.SetTag("exception.message", ``exception``.Message) |> ignore
+                          activity.SetTag("exception.stacktrace", ``exception``.StackTrace) |> ignore
+                          activity.Stop()
+                | _ ->
+                     let activity = this.activitySource.StartActivity(producer.Topic + " OnSendAcknowledgement",ActivityKind.Producer)                    
+                     if activity <> null then               
+                        if activity.IsAllDataRequested = true then
+                          activity.SetTag("messaging.destination_kind", "topic") |> ignore
+                          activity.SetTag("messaging.destination", producer.Topic) |> ignore
+                          activity.SetTag("messaging.operation", "OnSendAcknowledgement") |> ignore
+                          activity.SetTag("messaging.message_id", messageId) |> ignore       
+                          activity.Stop()  
+          
+                   
---
+++ src/Pulsar.Client.Otel/OtelConsumerInterceptor.fs
@@ -0,0 +1,51 @@
+﻿module Pulsar.Client.Otel.OtelConsumerInterceptor
+
+open System
+open System.Collections.Concurrent
+open System.Diagnostics
+open Pulsar.Client.Api
+open Pulsar.Client.Common
+
+type OTelConsumerInterceptor<'T>() =
+    let endActivity(consumer:IConsumer<'T>, messageID:MessageId, ``exception``:Exception, ackType, 
+                            act : ActivitySource)=              
+        let activity = act.StartActivity(consumer.Topic + " consumed",ActivityKind.Consumer)                
+        match ``exception`` with
+                | null ->
+                    activity.SetTag("acknowledge.type", ackType) |> ignore
+                    activity.SetTag("messaging.destination_kind", "topic") |> ignore
+                    activity.SetTag("messaging.destination", consumer.Topic) |> ignore
+                    activity.SetTag("messaging.message_id", messageID) |> ignore
+                    activity.SetTag("messaging.operation", "AfterConsume") |> ignore
+                | _ ->
+                    activity.SetTag("exception.type", ``exception``.Source) |> ignore
+                    activity.SetTag("exception.message", ``exception``.Message) |> ignore
+                    activity.SetTag("exception.stacktrace", ``exception``.StackTrace) |> ignore
+        activity.Stop()          
+        ()
+      
+    member this.activitySource : ActivitySource = new ActivitySource("pulsar.consumer")
+    
+    interface IConsumerInterceptor<'T> with
+        member this.BeforeConsume(consumer, message) =           
+            let activity = this.activitySource.StartActivity(consumer.Topic + " receive",ActivityKind.Consumer)
+            if activity <> null then                
+                if activity.IsAllDataRequested = true then                   
+                   activity.SetTag("messaging.system", "pulsar") |> ignore
+                   activity.SetTag("messaging.destination_kind", "topic") |> ignore
+                   activity.SetTag("messaging.destination", consumer.Topic) |> ignore
+                   activity.SetTag("messaging.operation", "BeforeConsume") |> ignore
+                   activity.Stop()
+                  //extract propagator here
+            message
+           
+           
+        member this.Close() = ()
+        member this.OnAckTimeoutSend(consumer, messageId) =
+            endActivity(consumer,messageId, null, "AcknowledgeType.Timeout",this.activitySource)
+        member this.OnAcknowledge(consumer, messageId, ``exception``) =
+            endActivity(consumer,messageId, ``exception``, "AcknowledgeType.Ok", this.activitySource)
+        member this.OnAcknowledgeCumulative(consumer, messageId, ``exception``) =
+            endActivity(consumer,messageId, ``exception``, "AcknowledgeType.Cumulative", this.activitySource)
+        member this.OnNegativeAcksSend(consumer, messageId) =
+            endActivity(consumer,messageId, null, "AcknowledgeType.Negative", this.activitySource)
