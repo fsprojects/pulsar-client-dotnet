669206c60c35c8b9427f784e46a718c820f2ef9f
Vladimir Shchur <odindafna2006@rambler.ru>
2023-09-27 09:41:06 -0700

    Improved CRC32 to be calculated by ulong batches

--- src/Pulsar.Client.Proto/Crc32c.cs
+++ src/Pulsar.Client.Proto/Crc32c.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Numerics;
 using Microsoft.IO;
 
@@ -14,25 +15,41 @@ namespace Pulsar.Client.Common
             foreach (var memory in memorySequence)
             {
                 var span = memory.Span;
-                var currentBlockLength = span.Length;
-                var i = 0;
-                while (size-- > 0 && i < currentBlockLength)
-                {
-                    crc = BitOperations.Crc32C(crc, span[i]);
-                    i++;
-                }
+                crc = CrcAlgorithm(size, span, crc);
             }
             return crc ^ ~0U; //0xFFFFFFFF
+        }
+
+        private static uint CrcAlgorithm(int size, ReadOnlySpan<byte> span, uint crc)
+        {
+            var currentBlockLength = span.Length;
+            var i = 0;
+            var bigStepsCount = currentBlockLength / 8;
+            while (i < bigStepsCount)
+            {
+                var start = i * 8;
+                var batch = BitConverter.ToUInt64(span.Slice(start, 8));
+                crc = BitOperations.Crc32C(crc, batch);
+                i++;
+            }
+
+            i = bigStepsCount * 8;
+            while (size > 0 && i < currentBlockLength)
+            {
+                crc = BitOperations.Crc32C(crc, span[i]);
+                size--;
+                i++;
+            }
+            return crc;
         }
 
         internal static uint GetForMS(MemoryStream stream, int size)
         {
             var crc = ~0U; //0xFFFFFFFF
             var buf = stream.GetBuffer();
-            var offset = stream.Position;
-            for (var i = 0; i < size; i++)
-                crc = BitOperations.Crc32C(crc, buf[offset+i]);
-
+            var offset = (int) stream.Position;
+            var span = buf.AsSpan(offset);
+            crc = CrcAlgorithm(size, span, crc);
             return crc ^ ~0U; //0xFFFFFFFF
         }
     }
