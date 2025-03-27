b94abbc12ee22910981a225c07964afa59ec5f82
Zike Yang <zike@apache.org>
2025-03-27 23:47:17 +0800

    Split client TLS transport encryption from authentication (#302) (#303)
    * Split client TLS transport encryption from authentication
    
    * Address comments
    
    * Improve test
    
    * Fix CI
    
    * Fix CI
    
    * Fix CI
    
    * Address comments
    
    (cherry picked from commit 4c058e0f667548e83c0e208ab47ca550ec690948)

--- tests/IntegrationTests/Tls.fs
+++ tests/IntegrationTests/Tls.fs
@@ -4,6 +4,7 @@
 
 open System
 open Expecto
+open Serilog
 
 open System.Threading.Tasks
 open Pulsar.Client.Api
@@ -11,44 +12,48 @@ open Pulsar.Client.Common
 open Pulsar.Client.IntegrationTests.Common
 
 
+let tlsTransport (client:PulsarClient) testName =
+    testTask testName {
+        Log.Debug("Started " + testName)
+        let topicName = "public/default/topic-" + Guid.NewGuid().ToString("N")
+        let numberOfMessages = 10
+        let messageIds = ResizeArray<MessageId>()
+
+        let! (producer : IProducer<byte[]>) =
+            client.NewProducer()
+                .Topic(topicName)
+                .CreateAsync()
+
+        let! (consumer : IConsumer<byte[]>) =
+            client.NewConsumer()
+                .Topic(topicName)
+                .ConsumerName("concurrent")
+                .SubscriptionName("test-subscription")
+                .SubscribeAsync()
+
+        let producerTask =
+            Task.Run(fun () ->
+                task {
+                    do! produceMessages producer numberOfMessages "concurrent"
+                }:> Task)
+
+        let consumerTask =
+            Task.Run(fun () ->
+                task {
+                    for _ in 1..numberOfMessages do
+                        let! message = consumer.ReceiveAsync()
+                        messageIds.Add message.MessageId
+                    do! consumer.DisposeAsync()
+                }:> Task)
+
+        do! Task.WhenAll(producerTask, consumerTask)
+        Log.Debug("Finished " + testName)
+    }
+
 [<Tests>]
 let tests =
     testList "Tls" [
-        testTask "Tls transport" {
-            let client = getSslAdminClient()
-            let topicName = "public/default/topic-" + Guid.NewGuid().ToString("N")
-            let numberOfMessages = 10
-            let messageIds = ResizeArray<MessageId>()
-            
-            let! (producer : IProducer<byte[]>) =
-                client.NewProducer()
-                    .Topic(topicName)
-                    .CreateAsync() 
-            
-            let! (consumer : IConsumer<byte[]>) =
-                client.NewConsumer()
-                    .Topic(topicName)
-                    .ConsumerName("concurrent")
-                    .SubscriptionName("test-subscription")
-                    .SubscribeAsync()
-                    
-            let producerTask =
-                Task.Run(fun () ->
-                    task {
-                        do! produceMessages producer numberOfMessages "concurrent"
-                    }:> Task)
-
-            let consumerTask =
-                Task.Run(fun () ->
-                    task {
-                        for _ in 1..numberOfMessages do
-                            let! message = consumer.ReceiveAsync()
-                            messageIds.Add message.MessageId
-                        do! consumer.DisposeAsync()
-                    }:> Task)
-
-            do! Task.WhenAll(producerTask, consumerTask) 
-        }
-
+        tlsTransport (getSslAdminClient()) "Tls transport with mTls Authentication"
+        tlsTransport (getSslTokenClient()) "Tls transport with token Authentication"
     ]
 #endif
