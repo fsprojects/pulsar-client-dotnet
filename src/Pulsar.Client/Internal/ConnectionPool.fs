11f4500dd3eb260b6f4158e50dfd316806824fd0
Zike Yang <zike@apache.org>
2025-04-02 17:50:59 +0800

    Improve pipeline threshold setting

--- src/Pulsar.Client/Internal/ConnectionPool.fs
+++ src/Pulsar.Client/Internal/ConnectionPool.fs
@@ -134,7 +134,11 @@ type internal ConnectionPool (config: PulsarClientConfiguration) =
                                   broker, maxMessageSize)
         backgroundTask {
             let (PhysicalAddress physicalAddress) = broker.PhysicalAddress
-            let pipeOptions = PipeOptions(pauseWriterThreshold = max (int64 maxMessageSize) 65536 )
+            // We should use PipeOptions.Default.PauseWriterThreshold as the minimum value for pauseWriterThreshold. A value that's too small isn't practical and will affect performance.
+            let pauseWriterThreshold = max (int64 maxMessageSize) PipeOptions.Default.PauseWriterThreshold
+            // Make sure the resumeWriterThreshold is half of the pauseWriterThreshold for better performance.
+            let resumeWriterThreshold = int64 (pauseWriterThreshold / 2L)
+            let pipeOptions = PipeOptions(pauseWriterThreshold = pauseWriterThreshold, resumeWriterThreshold = resumeWriterThreshold )
             let! socket = getSocket physicalAddress
 
             try
