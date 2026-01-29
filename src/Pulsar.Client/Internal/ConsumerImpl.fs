170cb2fe57e9b08c6435270b02e9ed8116e0b6bb
Zike Yang <zike@apache.org>
2026-01-30 04:39:13 +0800

    Fix Reader HasMessageAvailable behavior after seeking by timestamp (#344)
    * Fix Reader HasMessageAvailable behavior after seeking by timestamp
    
    * Improve logic

--- src/Pulsar.Client/Internal/ConsumerImpl.fs
+++ src/Pulsar.Client/Internal/ConsumerImpl.fs
@@ -1097,6 +1097,7 @@ type internal ConsumerImpl<'T> (consumerConfig: ConsumerConfiguration<'T>, clien
                             hasSoughtByTimestamp <- true
                             Commands.newSeekByTimestamp consumerId requestId timestamp, MessageId.Earliest
                         | SeekType.MessageId messageId ->
+                            hasSoughtByTimestamp <- false
                             match messageId.ChunkMessageIds with
                             | Some chunkMessageIds when chunkMessageIds.Length >0 ->
                                     Commands.newSeekByMsgId consumerId requestId chunkMessageIds[0], chunkMessageIds[0]
@@ -1145,8 +1146,9 @@ type internal ConsumerImpl<'T> (consumerConfig: ConsumerConfiguration<'T>, clien
                                 let! lastMessageIdResult = getLastMessageIdAsync()
                                 let lastMessageId = lastMessageIdResult.LastMessageId
                                 // if the consumer is configured to read inclusive then we need to seek to the last message
-                                do! postAndAsyncReply this.Mb (fun channel ->
-                                                SeekAsync (SeekType.MessageId lastMessageId, channel))
+                                if consumerConfig.ResetIncludeHead && not hasSoughtByTimestamp then
+                                    do! postAndAsyncReply this.Mb (fun channel ->
+                                                    SeekAsync (SeekType.MessageId lastMessageId, channel))
                                 match lastMessageIdResult.MarkDeletePosition with
                                 | Some markDeletePosition ->
                                     if lastMessageId.EntryId < %0L then
