76f2081e166b0bb634f0b1fbb47929ec71deb4d8
Vladimir Shchur <odindafna2006@rambler.ru>
2026-03-12 18:25:43 -0700

    Refactoring

--- src/Pulsar.Client/Internal/EndPointResolver.fs
+++ src/Pulsar.Client/Internal/EndPointResolver.fs
@@ -22,3 +22,4 @@ type internal EndPointResolver(initialAddresses : Uri array) =
         if Array.isEmpty newAddresses then
             invalidArg "newAddresses" "Addresses list could not be empty."
         addresses <- newAddresses
+        currentIndex = -1
--- src/Pulsar.Client/Internal/HttpLookupService.fs
+++ src/Pulsar.Client/Internal/HttpLookupService.fs
@@ -11,6 +11,37 @@ open FSharp.UMX
 open System.Text.Json
 open Microsoft.Extensions.Logging
 open Pulsar.Client.Schema
+open System.Net.Http.Json
+open System.Text.Json.Serialization
+
+//  This class is mainly used for http lookup service
+//  We name this class `PulsarHttpClient` to avoid naming clash with native HttpClient, and in Java pulsar client it's just `HttpClient`
+type internal PulsarHttpClient (config: PulsarClientConfiguration) =
+
+    let authenticationDataProvider = config.Authentication.GetAuthData()
+
+    let jsonOptions = JsonSerializerOptions(
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+     )
+    do jsonOptions.Converters.Add(JsonStringEnumConverter())
+
+    let httpClient = new HttpClient(new SocketsHttpHandler(
+        PooledConnectionLifetime = TimeSpan.FromMinutes(2),
+        AllowAutoRedirect = true
+    ))
+
+    member this.Get<'T> (requestUri: string) =
+        backgroundTask {
+            if authenticationDataProvider.HasDataForHttp() then
+                let request = new HttpRequestMessage(HttpMethod.Get, requestUri)
+                for headerPropertyEntry in authenticationDataProvider.GetHttpHeaders() do
+                    request.Headers.Add(headerPropertyEntry.Key, headerPropertyEntry.Value)
+                let! response = httpClient.SendAsync(request)
+                response.EnsureSuccessStatusCode() |> ignore
+                return! response.Content.ReadFromJsonAsync<'T>(jsonOptions)
+            else
+                return! httpClient.GetFromJsonAsync<'T>(requestUri, jsonOptions)
+        }
 
 type internal HttpLookupService (config: PulsarClientConfiguration, _connectionPool: ConnectionPool) =
 
